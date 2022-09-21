41e5f8db234247894e86dd2afb3de9801137d60c
robjrdev <85550412+robjrdev@users.noreply.github.com>
2022-09-21 17:10:12 +0800

    Style hero section

--- src/assets/script.js
+++ src/assets/script.js
@@ -1,3 +1,22 @@
+"use strict";
+// -------- STICKY NAV --------------//
+
+window.onscroll = function () {
+  myFunction();
+};
+const header = document.querySelector(".header");
+
+const sticky = header.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
+
+// --------- HAMBURGER MENU -----------//
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".navbar-nav");
 
