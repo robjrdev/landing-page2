2eee9a69c079d1f0c918b14493bb4fa1611c2ba4
robjrdev <85550412+robjrdev@users.noreply.github.com>
2022-09-22 18:41:05 +0800

    Add carousel

--- src/assets/script.js
+++ src/assets/script.js
@@ -31,3 +31,56 @@ document.querySelectorAll(".nav-link").forEach((n) =>
     navMenu.classList.remove("active");
   })
 );
+
+// -----------SLIDER PRICING-----------///
+
+let sliderContainer = document.querySelector(".carousel");
+let innerSlider = document.querySelector(".slider");
+
+let pressed = false;
+let startX;
+let x;
+
+sliderContainer.addEventListener("mousedown", (e) => {
+  pressed = true;
+  startX = e.offsetX - innerSlider.offsetLeft;
+  sliderContainer.style.cursor = "grabbing";
+});
+
+sliderContainer.addEventListener("mouseenter", () => {
+  sliderContainer.style.cursor = "grab";
+});
+
+sliderContainer.addEventListener("mouseup", () => {
+  sliderContainer.style.cursor = "grab";
+  pressed = false;
+});
+
+sliderContainer.addEventListener("mousemove", (e) => {
+  if (!pressed) return;
+  e.preventDefault();
+
+  x = e.offsetX;
+});
+
+sliderContainer.addEventListener("mousemove", (e) => {
+  if (!pressed) return;
+  e.preventDefault();
+
+  x = e.offsetX;
+
+  innerSlider.style.left = `${x - startX}px`;
+});
+
+const checkBoundary = () => {
+  let outer = sliderContainer.getBoundingClientRect();
+  let inner = innerSlider.getBoundingClientRect();
+
+  if (parseInt(innerSlider.style.left) > 0) {
+    innerSlider.style.left = "0px";
+  }
+
+  if (inner.right < outer.right) {
+    innerSlider.style.left = `-${inner.width - outer.width}px`;
+  }
+};
